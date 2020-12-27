3f0828d73ff8e063fff9ab6b7addd1c9693b85d9
eichelb4rt <maurice.wenig@gmail.com>
2020-12-27 19:35:11 +0100

    more finetuning for elo

--- src/Config.ts
+++ src/Config.ts
@@ -1,7 +1,7 @@
 export default class Config {
 	static readonly lowerBoundElo = 800;
 	static readonly upperBoundElo = 3000;
-	static readonly lowerBoundK = 40;
+	static readonly lowerBoundK = 50;
 	static readonly upperBoundK = 10;
 	
 	static readonly publicCommandsDir = 'commands';
