ce8fee3d6d181220e85e01272fa75085e64a2a07
eichelb4rt <maurice.wenig@gmail.com>
2021-01-03 17:34:49 +0100

    ignored testing

--- src/testing.ts
+++
@@ -1,6 +0,0 @@
-import { TwoPlayerGame, Player, Winner } from "./Elo";
-let a: Player = new Player(2800);
-let b: Player = new Player(2400);
-let game = new TwoPlayerGame(a, b);
-game.report(Winner.PlayerB);
-console.log(a.getElo(), b.getElo());
