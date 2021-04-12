19f00e4aadbc9c16ce06a753775703f461098af9
eichelb4rt <maurice.wenig@gmail.com>
2021-04-12 23:59:28 +0200

    fix: init match_manager

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import Discord, { Message } from "discord.js";
 import Command from "./interfaces/Command";
 import CommandLoader from "./CommandLoader";
 import Config from "./Config";
+import MatchManager from "./matches/MatchManager";
 
 export const client = new Discord.Client();
 export const command_loader = new CommandLoader();
 
 export function main() {
+	setup();
 	client.once("ready", () => {
 		console.log("Ready!");
 	});
@@ -16,6 +18,10 @@ export function main() {
 	});
 	
 	client.login(Config.DISCORD_API_TOKEN);
+}
+
+function setup() {
+	const match_manager = MatchManager.getInstance();
 }
 
 if (require.main === module) {
